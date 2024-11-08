78057e8dee2df1b63393965f0509dc37d608e03a
tarik <tarik.id.10@hotmail.com>
2024-11-08 21:45:05 +0200

    added body validation

--- src/api/endpoints/delayedTaskRoutes.ts
+++ src/api/endpoints/delayedTaskRoutes.ts
@@ -1,18 +1,30 @@
 import { Request, Response, Router } from 'express';
 import { delayedTaskService } from '../../application';
 import { DelayedTask } from '../../domain/entities/DelayedTask';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 const delayedTaskRouter = Router()
 
-delayedTaskRouter.post('/delayedTask', async (req: Request, res: Response) => {
-    const { message, time } = req.body
+type DelayedTaskDto = {
+    message: string,
+    time: Date
+}
 
-    const delayedTask = new DelayedTask(message, new Date(time))
-    await delayedTaskService.scheduleTask(delayedTask)
-    console.log('new task has been scheduled');
+delayedTaskRouter.post('/delayedTask',
+    celebrate({
+        [Segments.BODY]: Joi.object().keys({
+            message: Joi.string().required(),
+            time: Joi.date().required()
+        })
+    }), async (req: Request, res: Response) => {
+        const { message, time } = req.body as DelayedTaskDto
 
-    res.status(200).send('task has been scheduled')
-})
+        const delayedTask = new DelayedTask(message, new Date(time))
+        await delayedTaskService.scheduleTask(delayedTask)
+        console.log('new task has been scheduled');
+
+        res.status(200).send('task has been scheduled')
+    })
 
 
 export default delayedTaskRouter 
--- src/api/endpoints/index.ts
+++ src/api/endpoints/index.ts
@@ -1,5 +1,6 @@
 import { Request, Response, Router } from "express";
 import delayedTaskRouter from "./delayedTaskRoutes";
+const { errors } = require('celebrate');
 
 const router = Router();
 
@@ -9,5 +10,6 @@ router.use((req: Request, res: Response) => {
     res.send('Not found')
 })
 
+router.use(errors());
 
 export default router 
